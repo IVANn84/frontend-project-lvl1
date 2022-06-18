8d4237d493fa277e63b0c4994760148954000243
Ivan Khristenko <ivan.tihii1984@gmail.com>
2022-06-18 12:42:43 +0300

    install lintr

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import receivingName from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
 receivingName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
+
 const receivingName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello,' + userName + '!');
+  console.log(`Hello,${userName}!`);
 };
+
 export default receivingName;
